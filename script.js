40e9712bdabf848be8737aaa461256f8c657f4fc
Ryan Manns <w.r.manns@outlook.com>
2021-07-20 16:27:40 -0400

    retooled the script

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-//Function is called when document is ready
+//Setting variables by class using jQuery
+var save = $('.save-btn');
+var timeBlock = $('.hour-segment');
+var currentTime = moment().hours();
+
+//Running function upon app start=up
 $(document).ready(function () {
-
-  //saveBtn event listener and functionality
-  $(".saveBtn").on("click", function () {
-    //Retrieve nearby values
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-
-    //Save to local storage
-    localStorage.setItem(time, value);
-  })
-
-
-  //Hour Updater
-  function hourUpdater() {
-    //Retrieves the current number of hours from moment.js
-    var currentHour = moment().hours();
-    //Loops over time blocks
-    $(".time-block").each(function () {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-      //Check to see if the current hour is past the Time Block's hour
-      //And add the "past" class if true
-      if (blockHour < currentHour) {
-        $(this).addClass("past");
-      }
-      //Checks the time block to see if it matches the current hour
-      //If true, remove "past" class and add "present" class
-      else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-      }
-      //If neither argument is true, removes any class and adds class of "future"
-      else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
-    });
-  }
-
-  hourUpdater();
-
-
-  //Checks Local Storage for saved data. If found, retrieves and displays
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
   //Format and display current day
   $(".currentDay").text(moment().format("dddd, MMMM Do"));
+
+  //Checks local storage for saved notes associated with the hour blocks
+  $("#9 .note").val(localStorage.getItem("9"));
+
+  $("#10 .note").val(localStorage.getItem("10"));
+
+  $("#11 .note").val(localStorage.getItem("11"));
+
+  $("#12 .note").val(localStorage.getItem("12"));
+
+  $("#13 .note").val(localStorage.getItem("13"));
+
+  $("#14 .note").val(localStorage.getItem("14"));
+
+  $("#15 .note").val(localStorage.getItem("15"));
+
+  $("#16 .note").val(localStorage.getItem("16"));
+
+  $("#17 .note").val(localStorage.getItem("17"));
+
+  function hourColor() {
+
+    //Iterating through the hour-block classes to check the hour ID against the current time
+    timeBlock.each(function() {
+        var hour = parseInt($(this).attr('id'));
+        if(hour === currentTime) {
+            $(this).addClass('current');
+        } else if(currentTime > hour) {
+            $(this).addClass('past');
+        }else {
+            $(this).addClass('future');
+        }
+    });
+  }
+
+  //Calling the hourColor function
+  hourColor();
+
+  //Save Button event listener
+  save.on("click", function () {
+    //Retrieve nearby values
+    var note = $(this).siblings(".note").val().trim();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, note);
+  })  
 });
